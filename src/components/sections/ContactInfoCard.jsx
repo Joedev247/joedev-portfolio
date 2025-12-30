6892e5af265de5735d7f9ef1b2f36677e84df025
joedev247 <joedev237@gmail.com>
2025-12-30 09:12:30 +0100

    about

--- src/components/sections/ContactInfoCard.jsx
+++ src/components/sections/ContactInfoCard.jsx
@@ -3,7 +3,7 @@ const ContactInfoCard = ({ Icon, title, content, link }) => (
     href={link}
     className="group flex items-start gap-4 p-4  bg-gray-900/30 border border-gray-800/50 hover:border-orange-500/50 transition-all duration-300"
   >
-    <div className="p-2  bg-gray-800/50 group-hover:bg-orange-500/10 transition-colors">
+    <div className="p-2 rounded-full bg-gray-800/50 group-hover:bg-orange-500/10 transition-colors">
       <Icon className="w-5 h-5 text-orange-500" />
     </div>
     <div>
